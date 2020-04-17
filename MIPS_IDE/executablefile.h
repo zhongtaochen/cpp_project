1f89e29c650d45a7994d25c98fd1f0ed99ef5347
zhongtaochen <40366606+zhongtaochen@users.noreply.github.com>
2020-04-17 21:51:03 +0800

    @zhongtaochen objectfile.h, executablefile.h

---
+++ MIPS_IDE/executablefile.h
@@ -0,0 +1,34 @@
+/**
+ * @file executablefile.h
+ * @brief <code>ExecutableFile</code> class
+ * @details This file exports an <code>ExecutableFile</code> class,
+ * adapting from the descriptions in Patterson and Henessy's book section 2.12.
+ */
+
+#ifndef EXECUTABLEFILE_H
+#define EXECUTABLEFILE_H
+
+#include "objectfile.h"
+#include <vector>
+
+/**
+ * @brief Output of designed <code>Linker</code> in this project.
+ * Records neccessary information for further simulating and debugging.
+ */
+class ExecutableFile {
+
+public:
+
+/**
+ * @brief Void initilizer for the <code>ExecutableFile</code> class.
+ * All attributes are public.
+ */
+    ExecutableFile(){};
+
+    unsigned int text_size;
+    unsigned int data_size;
+    std::vector<TextCell> text_segment;
+    std::vector<DataCell> data_segment;
+};
+
+#endif // EXECUTABLEFILE_H
---
+++ MIPS_IDE/objectfile.h
@@ -0,0 +1,70 @@
+/**
+ * @file objectfile.h
+ * @brief <code>ObjectFile</code> class, <code>TextCell</code> struct,
+ * <code>DataCell</code> struct, <code>RelocationCell</code> struct
+ * @details This file exports an <code>ObjectFile</code> class and several
+ * neccessary structs. The design adapts from the descriptions in Patterson
+ * and Henessy's book section 2.12.
+ */
+
+#ifndef OBJECTFILE_H
+#define OBJECTFILE_H
+
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <map>
+
+/**
+ * @brief Basic unit for text segment in both <code>ObjectFile</code> and
+ * <code>ExecutableFile</code> class.
+ */
+struct TextCell{
+    uint32_t address;
+    int32_t machine_code;
+    std::string instruction;
+};
+
+/**
+ * @brief Basic unit for data segment in both <code>ObjectFile</code> and
+ * <code>ExecutableFile</code> class.
+ */
+struct DataCell{
+    uint32_t address;
+    int32_t data;
+};
+
+/**
+ * @brief Basic unit for relocation information both <code>ObjectFile</code>.
+ */
+struct RelocationCell{
+    uint32_t address;
+    std::string instruciton_type;
+    std::string dependency;
+};
+
+/**
+ * @brief Output of designed <code>Assembler</code> in this project.
+ * Records neccessary information for further linking and debugging.
+ */
+class ObjectFile {
+
+public:
+
+/**
+ * @brief Void initilizer for the <code>ObjectFile</code> class.
+ * All attributes are public.
+ */
+    ObjectFile(){};
+
+    std::string name;
+    unsigned int text_size;
+    unsigned int data_size;
+    std::vector<TextCell> text_segment;
+    std::vector<DataCell> data_segment;
+    std::vector<RelocationCell> relocation_information;
+    std::map<std::string, std::string> symbol_table;
+
+};
+
+#endif // OBJECTFILE_H
