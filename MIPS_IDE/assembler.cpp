4abdd27bc17ea490b17f33f78bc0b4b14f392329
yahiuqu <117010220@link.cuhk.edu.cn>
2020-05-14 00:58:11 +0800

    Update assembler.cpp

--- MIPS_IDE/assembler.cpp
+++ MIPS_IDE/assembler.cpp
@@ -7,9 +7,9 @@
 #include <bitset>
 using namespace std;
 
-Assembler::Assembler(ifstream &asm_file):Assembler(fileToString(asm_file)){}
+Assembler::Assembler(ifstream& asm_file) :Assembler(fileToString(asm_file)) {}
 
-Assembler::Assembler(const string &asm_code) {
+Assembler::Assembler(const string& asm_code) {
     string::size_type data_pos = asm_code.find(".data");
     string::size_type text_pos = asm_code.find(".text");
     if (data_pos != asm_code.npos) {
@@ -31,23 +31,16 @@ void Assembler::handleDataSection(const string& asm_data_sec) {
         //        cout << intToHexString(obj_file.data_size) << endl;
         vector<string> tokens = split(line, "[ \t]+");
         string type = tokens.at(1);
-<<<<<<< HEAD
         string data_name = line.substr(0, line.find(":"));
         obj_file.symbol_table.insert({ "*" + data_name,obj_file.data_size });
 
-        if (type == ".asciiz"||type == ".ascii") {
+        if (type == ".asciiz" || type == ".ascii") {
             string str = line.substr(line.find("\"") + 1, line.rfind("\"") - line.find("\"") - 1);
             if (type == ".asciiz") {
                 str = str + "\0";
             }
-=======
-        string data_name = line.substr(0,line.find(":"));
-        if (type == ".asciiz") {
-            string str = line.substr(line.find("\"")+1, line.rfind("\"")-line.find("\"")-1);
-            str = str + "\0";
->>>>>>> ab5925c030a4594c9f05923b8bf45b6195f4ee30
             unsigned int word = 0;
-            for (unsigned int i =0; i < str.size(); i++) {
+            for (unsigned int i = 0; i < str.size(); i++) {
                 char ch = str.at(i);
                 word |= (ch << ((3 - (i % 4)) << 3));
                 uint32_t data = word;
@@ -62,44 +55,12 @@ void Assembler::handleDataSection(const string& asm_data_sec) {
                 obj_file.data_segment.push_back({ obj_file.data_size, data });
                 obj_file.data_size += 4;
             }
-<<<<<<< HEAD
         }
         else if (type == ".word") {
             string data = line.substr(line.find(".word") + 5);
-=======
-
-
-
-        }
-        else if (type == ".ascii") {
-            string str = line.substr(line.find("\"")+1, line.rfind("\"")-line.find("\"")-1);
-            // str = str + "\0";            ".ascii"的唯一区别是字符串末尾不加入终止符
-            unsigned int word = 0;
-            unsigned int i = 0;
-            for (; i < str.size(); i++) {
-                char ch = str.at(i);           //i作为序号去遍历索引str中的各字符，每个char 8个bits（单字节
-                word |= (ch << ((3 - (i % 4)) << 3)); //ch 进行位移操作
-                if (i % 4 == 3) {                   //每4个字节换行一次
-                    obj_file.data_segment.push_back({obj_file.data_size, intToBinaryString(word)});
-                    obj_file.data_size += 4;        //data_size即是地址
-                    word = 0;
-                }
-            }
-            if (str.size() % 4 != 0) {
-                obj_file.data_segment.push_back({obj_file.data_size, intToBinaryString(word)});
-                obj_file.data_size += 4;
-            }
-
-
-
-        }else if (type == ".word") {
-            string data = line.substr(line.find(".word")+5);
->>>>>>> ab5925c030a4594c9f05923b8bf45b6195f4ee30
             trim(data);
             vector<string> elements = split(data, "[ \t,]+");
-            obj_file.symbol_table.insert({"*"+data_name,obj_file.data_size});
             for (string element : elements) {
-<<<<<<< HEAD
                 uint32_t data = stoi(element, 0, 2);
                 obj_file.data_segment.push_back({ obj_file.data_size, data });
                 obj_file.data_size += 4;
@@ -139,57 +100,20 @@ void Assembler::handleDataSection(const string& asm_data_sec) {
             trim(data);
             for (int i = 0; i < stoi(data); i++) {
                 obj_file.data_segment.push_back({ obj_file.data_size, 00000000000000000000000000000000 });
-=======
-                obj_file.data_segment.push_back({obj_file.data_size, intToBinaryString(stoi(element))});
-                obj_file.data_size += 4;
-            }
-        }else if (type == ".half") {
-            string data = line.substr(line.find(".word")+5);
-            trim(data);
-            vector<string> elements = split(data, "[ \t,]+");
-            obj_file.symbol_table.insert({"*"+data_name,obj_file.data_size});
-            for (string element : elements) {
-                obj_file.data_segment.push_back({obj_file.data_size, intToBinaryString(stoi(element))});
-                obj_file.data_size += 2;
-            }
-            while (obj_file.data_size %4 != 3){
-                obj_file.data_segment.push_back({obj_file.data_size, 0000000000000000});
-                obj_file.data_size += 2;
-            }
-        }else if (type == ".byte") {
-            string data = line.substr(line.find(".word")+5);
-            trim(data);
-            vector<string> elements = split(data, "[ \t,]+");
-            obj_file.symbol_table.insert({"*"+data_name,obj_file.data_size});
-            for (string element : elements) {
-                obj_file.data_segment.push_back({obj_file.data_size, intToBinaryString(stoi(element))});
-                obj_file.data_size += 1;
-            }
-            while (obj_file.data_size %4 != 3){
-                obj_file.data_segment.push_back({obj_file.data_size, 00000000});
-                obj_file.data_size += 1;
-            }
-
-        }else if (type == ".space") {
-            string data = line.substr(line.find(".space")+6);
-            trim(data);
-            obj_file.symbol_table.insert({"*"+data_name,obj_file.data_size});
-            for(int i=0;i < stoi(data); i++){
-                obj_file.data_segment.push_back({obj_file.data_size, 00000000000000000000000000000000});
->>>>>>> ab5925c030a4594c9f05923b8bf45b6195f4ee30
                 obj_file.data_size += 4;
             }
         }
     }
 }
-void Assembler::handleTextSection(const string &asm_text_sec) {
+
+void Assembler::handleTextSection(const string& asm_text_sec) {
     preprocess(asm_text_sec);
     obj_file.text_size = asm_lines.size() << 2;
     scanLabels();
     assembleLines();
 }
 
-void Assembler::preprocess(const string &asm_code) {
+void Assembler::preprocess(const string& asm_code) {
     stringstream sstrm(asm_code);
     string asm_line;
     while (getline(sstrm, asm_line)) {
@@ -209,12 +133,13 @@ void Assembler::scanLabels() {
         if (pos != asm_line.npos) { // colon exists
             label = asm_line.substr(0, pos);
             remain = asm_line.substr(pos + 1);
-            obj_file.symbol_table.insert({label, i << 2});
+            obj_file.symbol_table.insert({ label, i << 2 });
             if (isEmptyLine(remain)) {
                 asm_lines.erase(asm_lines.begin() + i);
                 i--;
-                obj_file.text_size-=4;
-            } else {
+                obj_file.text_size -= 4;
+            }
+            else {
                 trim(remain);
                 asm_lines.at(i) = remain;
             }
@@ -225,13 +150,13 @@ void Assembler::scanLabels() {
 void Assembler::assembleLines() {
     for (unsigned int i = 0; i < asm_lines.size(); i++) {
         // this is i*2^2
-        obj_file.text_segment.push_back({i << 2,0,asm_lines.at(i)});
+        obj_file.text_segment.push_back({ i << 2,0,asm_lines.at(i) });
         LineAssembler line_assm(obj_file, i << 2);
         obj_file = line_assm.assemble();
     }
 }
 
-void Assembler::output(const string &file_path) {
+void Assembler::output(const string& file_path) {
     string mc_code = "";
     for (unsigned int i = 0; i < obj_file.text_segment.size(); i++) {
         mc_code += obj_file.text_segment.at(i).machine_code + "\n";
